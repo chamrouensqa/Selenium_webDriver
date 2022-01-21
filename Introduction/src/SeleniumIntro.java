06736d87e9e7b2970bcd309d3b35db4372cf0885
chamrouensqa <chamrouensann.edu@gmail.com>
2022-01-21 20:59:48 +0700

    Invoke the chrome driver

---
+++ Introduction/src/SeleniumIntro.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumIntro {
+	public static void main(String[] args) {
+		//Invoking Browser
+		//Invoke the Chrome browser
+		//Create the object of the class
+		
+		//chromedriver.exe -> Chrome Brower
+		WebDriver driver = new ChromeDriver();
+		
+		
+	}
+}
